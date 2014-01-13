0abfad74c900479419a44cece3216e899dde4bc2
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-13 00:04:56 +0000

    Added a config object to factory. So when we call getInstance the factory gives this object to new instance of client

--- src/main/java/pt/ist/fenixedu/android/FenixEduAndroidClientFactory.java
+++ src/main/java/pt/ist/fenixedu/android/FenixEduAndroidClientFactory.java
@@ -2,24 +2,46 @@ package pt.ist.fenixedu.android;
 
 import pt.ist.fenixedu.sdk.FenixEduConfig;
 
+/**
+ * A factory for creating FenixEduAndroidClient objects.
+ */
 public class FenixEduAndroidClientFactory {
 
+	/** The instance. */
 	private static FenixEduAndroidClient instance;
 
+	/**
+	 * The instance config. This config object will be passed to instance in
+	 * getInstance call
+	 */
+	private static FenixEduConfig instanceConfig;
+
+	/**
+	 * Instantiates a new fenix edu android client factory.
+	 */
 	private FenixEduAndroidClientFactory() {
 	}
 
+	/**
+	 * Gets the single instance of FenixEduAndroidClientFactory.
+	 * 
+	 * @return single instance of FenixEduAndroidClientFactory
+	 */
 	public static FenixEduAndroidClient getInstance() {
 		if (instance == null) {
-			instance = new FenixEduAndroidClient();
+			instance = new FenixEduAndroidClient(instanceConfig);
 		}
 		return instance;
 	}
 
+	/**
+	 * Sets the client config.
+	 * 
+	 * @param config
+	 *            the new client config
+	 */
 	public static void setClientConfig(FenixEduConfig config) {
-		// In case you call this first instead of getInstance
-		FenixEduAndroidClient client = getInstance();
-		client.setConfig(config);
+		instanceConfig = config;
 	}
 
 }
